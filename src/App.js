5921cdbf11b6e5bf91456c032e285c2cc7cf7ca3
lesley <lesleybanadzem2017@gmail.com>
2020-09-29 22:37:14 -0700

    Created files structure and folders for the app

--- src/App.js
+++ src/App.js
@@ -4,13 +4,7 @@ import { Card } from "react-bootstrap";
 import "./App.css";
 
 function App() {
-	return (
-		<main className="App">
-			<Card>
-				<Card.Body>This is some text within a card body.</Card.Body>
-			</Card>
-		</main>
-	);
+	return <main className="App"></main>;
 }
 
 export default App;
