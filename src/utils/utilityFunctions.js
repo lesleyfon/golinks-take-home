b5532400e47050d62bc785e980c9f8844aa2f538
lesley <lesleybanadzem2017@gmail.com>
2020-09-30 08:34:50 -0700

    1. Wrote a utility funtion to parse date and return it in a nice format 2. Added some more fields to be returnd in the relevant data to be displayed on the cards'

--- src/utils/utilityFunctions.js
+++ src/utils/utilityFunctions.js
@@ -1,4 +1,9 @@
 import Axios from "axios";
+
+/**
+ *
+ * @param {*} orgName Name of organisation to Fetch
+ */
 export async function fetchOrganizationRepos(orgName) {
 	try {
 		const response = await Axios(`https://api.github.com/orgs/${orgName}/repos`);
@@ -7,6 +12,15 @@ export async function fetchOrganizationRepos(orgName) {
 	} catch (error) {
 		return error;
 	}
+}
+
+/**
+ *
+ * @param {*} datestring String from response to parse ot ISO strig
+ */
+function parseDate(datestring) {
+	const event = new Date(datestring);
+	return `${event.getMonth()}/${event.getDate()}/${event.getFullYear()}`;
 }
 
 // Try returning just the data you need fom here
@@ -18,8 +32,11 @@ function sortData(data) {
 		id: repoInfo.id,
 		name: repoInfo.name,
 		language: repoInfo.language,
+		description: repoInfo.description,
 		star_count: repoInfo.stargazers_count,
 		fork_count: repoInfo.forks_count,
+		created_at: parseDate(repoInfo.created_at),
+		updated_at: parseDate(repoInfo.updated_at),
 	}));
 	return data;
 }
