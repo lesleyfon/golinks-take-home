cde870a0980009de7c26eb6e0bf039980698db59
lesley <lesleybanadzem2017@gmail.com>
2020-09-30 00:27:39 -0700

    Rendered the OrgRepoList in the OrgRepo page

--- src/pages/OrgRepos.js
+++ src/pages/OrgRepos.js
@@ -1,13 +1,19 @@
-import React, { useContext } from "react";
-import AppContext from "./../context/AppContext.js";
+import React from "react";
+
+import OrgReposList from "./../components/OrgReposList.js";
 /**
  * Page for displaying organization's repositories
  */
 function OrgRepos() {
 	// Accessing the context Store
-	const { orgName } = useContext(AppContext);
 
-	return <section> To display List of All repos</section>;
+	return (
+		<section>
+			{" "}
+			To display List of All repos
+			<OrgReposList />
+		</section>
+	);
 }
 
 export default OrgRepos;
